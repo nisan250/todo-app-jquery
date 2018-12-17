f1dcb4d4245af3cf5275358e55033b0fdaa8f119
nisan250 <nisan250@gmail.com>
2018-12-17 09:44:55 +0000

    adding git ignore and build for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
-var gulp = require('gulp'),
-    webserver = require('gulp-webserver');
+const gulp = require('gulp');
+const webserver = require('gulp-webserver');
+const runSequence = require('run-sequence');
+const concatCss = require('gulp-concat-css');
+const browserSync = require('browser-sync').create();
 
 var builds = 'app';
 
@@ -31,3 +34,44 @@ gulp.task('webserver', function() {
 });
 
 gulp.task('default', ['watch', 'html', 'js', 'css', 'webserver']);
+
+
+
+//--build--//
+
+gulp.task('copyAllHTML', function() {
+    gulp.src(path + '/**/*.html')
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('minifyImages', () => {
+    gulp.src(path + '/imgs/**/*')
+    .pipe(imgmin())
+    .pipe(gulp.dest('dist/imgs'));
+});
+
+gulp.task('processJS', function() {
+    gulp.src(path + '/js/**/*.js')
+    .pipe(uglify())
+    .pipe(concat('application.js'))
+    .pipe(gulp.dest(('dist/js')))
+});
+// .pipe(browserSync.stream()) - insurers browser always running the current css when 
+gulp.task('sass2CSS', function() {
+    return gulp.src(path + '/sass/**/*.scss')
+                .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+                .pipe(gulp.dest('src/css'))
+                .pipe(browserSync.stream())
+});
+
+gulp.task('processCss', function() {
+    return gulp.src(path + '/css/**/*.css')
+                .pipe(concatCss('styles.css'))
+                .pipe(gulp.dest('dist/css'))
+                .pipe(browserSync.stream())
+});
+
+gulp.task('build', function(callback) {
+    runSequence(['copyAllHTML', 'processJS', 'processCss'], callback);
+    // runSequence('sass2CSS', ['copyAllHTML', 'minifyImages', 'processJS', 'processCss'], callback);
+});
