bdd8517c85f23f34f699dc609c68776e0e826a57
nisan250 <nisan250@gmail.com>
2018-12-18 14:13:08 +0000

    working with data

--- tmp/js/script.js
+++ tmp/js/script.js
@@ -1,16 +1,40 @@
-console.log('script.js');
-// $( document ).ready(function() {
-//   console.log( "ready!" );
-// });
+$(function(){
 
-$('.todo-addheading').on('click', function() {
-  $('.card-body').toggle(300);
-});
+  var todoData ;
 
-$.ajax({
-  url: 'js/data.json'
-}).done(function(data) {
-  $('#field-todoTitle').text(data[0].todoTitle);
-  $('#field-todoOwner').text(data[0].todoOwner);
-  $('#field-todoDescription').text(data[0].todoNote);
-});
+  function removeTodo (todoID) {
+    var theTodo = _.find(todoData, function(item) {
+      return item.id == todoID;
+    })
+    todoData = _.without(todoData, theTodo);
+  }
+  $('.todo-addheading').on('click', function() {
+    $('.card-body').toggle(300);
+  });
+  
+  $.ajax({
+    url: 'js/data.json',
+    method: 'GET',
+    data: {}
+  }).done(function(data) {
+    todoData = data;
+    $('#todoList').loadTemplate('todo-list.html', data, {
+      complete: function() {
+        $('.todo-delete').on('click', function() {
+          $(this).parents('.todo-item').hide(300, function() {
+            var theItem = $(this).attr('id');
+            // data.splice(Number(theItem), 1);
+            removeTodo(theItem);
+            console.log(todoData);
+            $(this).remove();
+          });
+        });
+      }
+    });
+  });
+
+
+
+
+})
+
